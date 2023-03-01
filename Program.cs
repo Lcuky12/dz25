06641bef3334296fcad3a67a99a92ef3eee2e0d9
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 14:56:00 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int maxNumber = rand.Next(0, 101);
+            int sum = 0;
+
+            for ( int i = 3; i <= maxNumber; i++ ) 
+            {
+
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;                 
+                }
+                
+            }
+            Console.WriteLine(" Рандомное число " + maxNumber + " Сумма всех чисел кратных 3 или 5 до " + maxNumber + " равна " + sum);
+        }
+    }
+}
